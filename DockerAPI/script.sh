a1e2d7ef7ec4808a1da72d4454935db6a4fe06ab
NourCN <chamas_n@etna-alternance.net>
2022-01-13 18:35:12 +0100

    Kube

--- DockerAPI/script.sh
+++ DockerAPI/script.sh
@@ -4,5 +4,6 @@ docker commit dockerapi_api_1 nourcn/orchestration:api
 docker push nourcn/orchestration:api
 docker commit dockerapi_front_1 nourcn/orchestration:front
 docker push nourcn/orchestration:front
+minikube start
 kubectl apply -f deployment.yaml
 minikube dashboard
