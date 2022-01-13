9561ff8789328ee716f2397f6adc5fd665d68bdb
NourCN <chamas_n@etna-alternance.net>
2022-01-13 16:21:33 +0100

    13

--- DockerAPI/front/src/App.js
+++ DockerAPI/front/src/App.js
@@ -1,15 +1,38 @@
-import './App.css';
+import "./App.css";
 
 //Object { _id: "61d455e68c48231dde71e861", auteur: "kuba", message: "salut tt le monde", … }
-
 function App() {
-  const str = fetch('http://localhost:4242').then(res => {return res.json()});
-  console.log(str);
-  return (
-    <div className="App">
-      <h1>Front pour projet Orchestration de services</h1>
-    </div>
-  );
+	const str = fetch("http://localhost:4242")
+		.then((res) => res.json())
+		.then((res) =>
+    {
+      let tabBody = document.querySelector("tbody");
+    
+    for (const user of res) {
+      tabBody.innerHTML += `
+        <tr>
+        <td>${user.auteur}</td>
+        <td>${user.message}</td> 
+        </tr>
+        `;
+    } });
+
+	return (
+		<div className="App">
+			<h1>Front pour projet Orchestration de services</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Auteur</th>
+            <th>Message</th>
+          </tr>
+        </thead>
+        <tbody>
+         
+        </tbody>
+      </table>
+		</div>
+	);
 }
 
 export default App;
--- DockerAPI/script.sh
+++ DockerAPI/script.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
-sudo docker-compose up --build
+docker-compose up --build
