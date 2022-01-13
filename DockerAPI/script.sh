e68daef16474ad01a26e8440911b3c78a3a36590
NourCN <chamas_n@etna-alternance.net>
2022-01-13 17:32:16 +0100

    yaml

--- DockerAPI/script.sh
+++ DockerAPI/script.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-docker-compose up --build
+docker-compose up --build -d
+docker commit dockerapi_api_1 nourcn/orchestration:api
+docker push nourcn/orchestration:api
+docker commit dockerapi_front_1 nourcn/orchestration:front
+docker push nourcn/orchestration:front
+kubectl apply -f deployment.yaml
+minikube dashboard
